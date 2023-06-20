ad1d6a9ee02d4e71d9d3c82c0e1509ee71330879
peter910820 <peter910820@gmail.com>
2023-06-20 23:02:28 +0800

    add config file

--- rpc_connect.py
+++ rpc_connect.py
@@ -1,18 +1,25 @@
 from pypresence import Presence
+import json, os
 import time
 
-client_id = "1120361058729738250"
+print([file_name for file_name in os.listdir("./config")])
+
+use = input("choose a config file: ")
+
+with open(f"./config/{use}.json",'r', encoding='utf8')  as f:
+    data = json.loads(f.read())
+
+client_id = data["CLIENT_ID"]
 RPC = Presence(client_id)
 
 RPC.connect()
 
 RPC.update(
-        state= f"正在玩迷霧列車",
-        large_image= "cover",
-        small_image = "cover",
+        state= data["STATE"],
+        large_image= data["IMG_NAME"],
+        small_image= data["IMG_NAME"],
         start = int(time.time())
     )
-
 
 while True:
     time.sleep(60)
