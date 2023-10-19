48c9dd1266fd9ed2c4d140e9fc6f63b81b50333d
peter910820 <peter910820@gmail.com>
2023-10-20 02:17:31 +0800

    upload first formal version(1.0)

--- app.py
+++ app.py
@@ -2,27 +2,49 @@ import eel, time, asyncio
 from pypresence import Presence
 
 RPC = None
+discord_img = "https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png"
 
 @eel.expose
-def connect_rpc(client_id):  
-    global RPC   
-    RPC = Presence(client_id)
-    RPC.connect()
+def connect_rpc(client_id):
+    global RPC
+    try:
+        RPC = Presence(client_id)
+        RPC.connect()
+    except Exception as err:
+        print(err)
     
 @eel.expose
-def update(details, state, img_url, little_img):    
-
-     RPC.update(
-            state= state,
-            details= details,
-            large_image= img_url,
-            small_image= little_img,
-            start = int(time.time())
-        )
+def update(deliver):
+    empty_keys = [key for key, value in deliver.items() if not value]
+    for e in empty_keys:
+        match e:
+            case "details":
+                deliver["details"] = "Test"
+            case "state":
+                deliver["state"] = None
+            case "img_url":
+                deliver["img_url"] = discord_img
+            case "little_img":
+                deliver["little_img"] = discord_img
+    try:
+        print(deliver)
+        print(type(deliver))
+        RPC.update(
+                state= deliver["state"],
+                details= deliver["details"],
+                large_image= deliver["img_url"],
+                small_image= deliver["little_img"],
+                start = int(time.time())
+            )
+    except Exception as err:
+        print(err)
 
 @eel.expose
-def disconnect_rpc(client_id): 
-    RPC.close()
+def disconnect_rpc():
+    try: 
+        RPC.close()
+    except Exception as err:
+        print(err)
 
 eel.init('interface')
-eel.start('/main.html',size = (400,600), position = (400, 100))
+eel.start('/main.html',size = (600,600), position = (400, 100))
--- interface/main.js
+++ interface/main.js
@@ -3,21 +3,35 @@ async function connect(){
     result = await eel.connect_rpc(client_id)();
     let inputElements = document.getElementsByTagName('input');
     for(let i = 0; i<inputElements.length;i++){
-        console.log(i);
         inputElements[i].disabled = false;
     }
+    let disconnect_button = document.getElementById('disconnect_button');
+    let connect_button = document.getElementById('connect_button');
+    disconnect_button.setAttribute('class','waves-effect waves-light btn-flat');
+    connect_button.setAttribute('class','waves-effect waves-light btn-flat disabled');
 }
-async function update(){ 
-    let client_id = document.getElementById('client_id').value;
-    let details = document.getElementById('details').value;
-    let state = document.getElementById('state').value;
-    let img_url = document.getElementById('img_url').value;
-    let little_img = document.getElementById('little_img').value;
-    result = await eel.update(details, state, img_url, little_img)();
+async function update(){
+    let deliver = new Map();
+    deliver['details'] = document.getElementById('details').value;
+    deliver['state'] = document.getElementById('state').value;
+    deliver['img_url'] = document.getElementById('img_url').value;
+    deliver['little_img'] = document.getElementById('little_img').value;
+
+    result = await eel.update(deliver)();
 }
 async function disconnect(){ 
-    result = await eel.disconnect_rpc(client_id)();
-    
-    //最後將返回的值設定在HTML上的<p>內
-    document.querySelector('p').textContent = "close";
+    result = await eel.disconnect_rpc()();
+
+    let disconnect_button = document.getElementById('disconnect_button');
+    let connect_button = document.getElementById('connect_button');
+    disconnect_button.setAttribute('class','waves-effect waves-light btn-flat disabled');
+    connect_button.setAttribute('class','waves-effect waves-light btn-flat');
+
+    let inputElements = document.getElementsByTagName('input');
+    for(let i = 1; i<inputElements.length;i++){
+        console.log(i);
+        inputElements[i].disabled = true;
+    }
+
+    // document.querySelector('p').textContent = "close";
 }
