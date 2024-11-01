bc6f6a0e00b79e7be1721410d7a2dd6da59bf0a9
peter910820 <peter910820@gmail.com>
2024-11-02 02:19:03 +0800

    modify typesetting

--- app.py
+++ app.py
@@ -3,49 +3,60 @@ import time
 
 from pypresence import Presence
 
-RPC = None
-discord_img = "https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png"
+rpc = None
+discord_img = 'https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png'
+
+
+@eel.expose
+def start_app():
+    eel.init('interface')
+    eel.start('/main.html', size=(600, 600), position=(400, 100))
+
 
 @eel.expose
 def connect_rpc(client_id):
-    global RPC
+    global rpc
     try:
-        RPC = Presence(client_id)
-        RPC.connect()
+        rpc = Presence(client_id)
+        rpc.connect()
     except Exception as err:
         print(err)
-    
+
+
 @eel.expose
 def update(deliver):
-    empty_keys = [key for key, value in deliver.items() if not value]
-    for e in empty_keys:
-        match e:
-            case "details":
-                deliver["details"] = "Test"
-            case "state":
-                deliver["state"] = None
-            case "img_url":
-                deliver["img_url"] = discord_img
-            case "little_img":
-                deliver["little_img"] = discord_img
     try:
+        empty_keys = [key for key, value in deliver.items() if not value]
+        for e in empty_keys:
+            match e:
+                case 'details':
+                    deliver['details'] = 'Test'
+                case 'state':
+                    deliver['state'] = None
+                case 'img_url':
+                    deliver['img_url'] = discord_img
+                case 'little_img':
+                    deliver['little_img'] = discord_img
+
         print(deliver)
-        RPC.update(
-                state= deliver["state"],
-                details= deliver["details"],
-                large_image= deliver["img_url"],
-                small_image= deliver["little_img"],
-                start = int(time.time())
-            )
+        rpc.update(
+            state=deliver['state'],
+            details=deliver['details'],
+            large_image=deliver['img_url'],
+            small_image=deliver['little_img'],
+            start=int(time.time())
+        )
     except Exception as err:
         print(err)
+
 
 @eel.expose
 def disconnect_rpc():
-    try: 
-        RPC.close()
+    try:
+        rpc.close()
     except Exception as err:
         print(err)
 
-eel.init('interface')
-eel.start('/main.html',size = (600,600), position = (400, 100))
+
+if __name__ == '__main__':
+    start_app()
--- interface/main.js
+++ interface/main.js
@@ -1,37 +1,51 @@
-async function connect(){ 
-    let client_id = document.getElementById('client_id').value;
-    result = await eel.connect_rpc(client_id)();
-    let inputElements = document.getElementsByTagName('input');
-    for(let i = 0; i<inputElements.length;i++){
-        inputElements[i].disabled = false;
+async function connect() {
+  let client_id = document.getElementById("client_id").value;
+  await eel.connect_rpc(client_id)();
+
+  if (!client_id) {
+    alert("client_id error");
+    return;
+  } else {
+    let inputElements = document.getElementsByTagName("input");
+    for (let i = 0; i < inputElements.length; i++) {
+      inputElements[i].disabled = false;
     }
-    let disconnect_button = document.getElementById('disconnect_button');
-    let connect_button = document.getElementById('connect_button');
-    disconnect_button.setAttribute('class','waves-effect waves-light btn-flat');
-    connect_button.setAttribute('class','waves-effect waves-light btn-flat disabled');
+    let disconnect_button = document.getElementById("disconnect_button");
+    let connect_button = document.getElementById("connect_button");
+    disconnect_button.setAttribute(
+      "class",
+      "waves-effect waves-light btn-flat"
+    );
+    connect_button.setAttribute(
+      "class",
+      "waves-effect waves-light btn-flat disabled"
+    );
+  }
 }
-async function update(){
-    let deliver = new Map();
-    deliver['details'] = document.getElementById('details').value;
-    deliver['state'] = document.getElementById('state').value;
-    deliver['img_url'] = document.getElementById('img_url').value;
-    deliver['little_img'] = document.getElementById('little_img').value;
+async function update() {
+  let deliver = new Map();
+  deliver["details"] = document.getElementById("details").value;
+  deliver["state"] = document.getElementById("state").value;
+  deliver["img_url"] = document.getElementById("img_url").value;
+  deliver["little_img"] = document.getElementById("little_img").value;
 
-    result = await eel.update(deliver)();
+  await eel.update(deliver)();
 }
-async function disconnect(){ 
-    result = await eel.disconnect_rpc()();
+async function disconnect() {
+  await eel.disconnect_rpc()();
+  let disconnect_button = document.getElementById("disconnect_button");
+  let connect_button = document.getElementById("connect_button");
+  disconnect_button.setAttribute(
+    "class",
+    "waves-effect waves-light btn-flat disabled"
+  );
+  connect_button.setAttribute("class", "waves-effect waves-light btn-flat");
 
-    let disconnect_button = document.getElementById('disconnect_button');
-    let connect_button = document.getElementById('connect_button');
-    disconnect_button.setAttribute('class','waves-effect waves-light btn-flat disabled');
-    connect_button.setAttribute('class','waves-effect waves-light btn-flat');
+  let inputElements = document.getElementsByTagName("input");
+  for (let i = 1; i < inputElements.length; i++) {
+    console.log(i);
+    inputElements[i].disabled = true;
+  }
 
-    let inputElements = document.getElementsByTagName('input');
-    for(let i = 1; i<inputElements.length;i++){
-        console.log(i);
-        inputElements[i].disabled = true;
-    }
-
-    // document.querySelector('p').textContent = "close";
+  // document.querySelector('p').textContent = "close";
 }
